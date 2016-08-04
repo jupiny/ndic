ae3d011d152daf99d85f5fb88493b26bdcc7e79d
jupiny <tmdghks584@gmail.com>
2016-08-04 19:42:03 +0900

    fix ndic script: echo ConnectionError message

--- ndic/scripts/ndic.py
+++ ndic/scripts/ndic.py
@@ -13,7 +13,8 @@ def ndic(search_word):
     try:
         response = requests.get(naver_dict_url+search_word)
     except requests.ConnectionError:
-        return "Network connection is lost. Please check the connection to the Internet."
+        click.echo("Network connection is lost. Please check the connection to the Internet.")
+        return
     dom = BeautifulSoup(response.content, "html.parser")
     search_word_element = dom.select_one(".fnt_e30") or None
     word_meaning_element = dom.select_one(".fnt_k05") or None
