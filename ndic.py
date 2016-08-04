731155ca6686a59f57c6ea369d280881b83c899b
jupiny <tmdghks584@gmail.com>
2016-08-04 15:12:46 +0900

    add requests exception handling

--- ndic.py
+++ ndic.py
@@ -5,7 +5,10 @@ from bs4 import BeautifulSoup
 def ndic(search_word):
 
     naver_dict_url = "http://endic.naver.com/search.nhn?sLn=kr&searchOption=all&query="
-    response = requests.get(naver_dict_url+search_word)
+    try:
+        response = requests.get(naver_dict_url+search_word)
+    except requests.ConnectionError:
+        return "Network connection is lost. Please check the connection to the Internet."
     dom = BeautifulSoup(response.content, "html.parser")
     search_word_element = dom.select_one(".fnt_e30") or None
     word_meaning_element = dom.select_one(".fnt_k05") or None
