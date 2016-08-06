beacd02c9655367f43998be50370039dcf099c4a
jupiny <tmdghks584@gmail.com>
2016-08-06 18:22:59 +0900

    fix setup to handle sub-packages: import find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
   name = 'ndic',
-  packages = ['ndic'],
+  # packages = ['ndic'],
+  packages=find_packages(),
+  include_package_data=True,
   # py_modules=['ndic'],
   install_requires = [
     'requests',
@@ -11,12 +13,12 @@ setup(
     'click',
     'nose',
   ],
-  version = '0.7',
+  version = '0.9',
   description = 'Python module for NAVER English-Korean and Korean-English dictionaries',
   author = 'jupiny',
   author_email = 'tmdghks584@gmail.com',
   url = 'https://github.com/jupiny/ndic',
-  download_url = 'https://github.com/jupiny/ndic/tarball/0.7',
+  download_url = 'https://github.com/jupiny/ndic/tarball/0.9',
   # keywords = ['dictionary', 'translate', 'English', 'Korean', 'Naver'],
   classifiers = [],
   test_suite='nose.collector',
