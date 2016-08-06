c8af62baa0aef6fd479cd1f0b7e2fbe45c5e3018
jupiny <tmdghks584@gmail.com>
2016-08-06 19:44:17 +0900

    init requirements

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 
+def get_requirements(filename):
+    return open(filename).read().splitlines()
+
+install_requires = get_requirements('requirements.txt')
+
 setup(
-  name = 'ndic',
+  name='ndic',
   # packages = ['ndic'],
   packages=find_packages(),
   include_package_data=True,
   # py_modules=['ndic'],
-  install_requires = [
-    'requests',
-    'beautifulsoup4',
-    'click',
-    'nose',
-  ],
-  version = '0.9',
-  description = 'Python module for NAVER English-Korean and Korean-English dictionaries',
-  author = 'jupiny',
-  author_email = 'tmdghks584@gmail.com',
-  url = 'https://github.com/jupiny/ndic',
-  download_url = 'https://github.com/jupiny/ndic/tarball/0.9',
+  install_requires=install_requires,
+  version='0.9',
+  description='Python module for NAVER English-Korean and Korean-English dictionaries',
+  author='jupiny',
+  author_email='tmdghks584@gmail.com',
+  url='https://github.com/jupiny/ndic',
+  download_url='https://github.com/jupiny/ndic/tarball/0.9',
   # keywords = ['dictionary', 'translate', 'English', 'Korean', 'Naver'],
-  classifiers = [],
+  classifiers=[],
   test_suite='nose.collector',
   tests_require=['nose'],
   entry_points='''
