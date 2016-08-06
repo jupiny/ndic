5c7de2dcc589bd8aa2debe285e9124bf59754e90
jupiny <tmdghks584@gmail.com>
2016-08-06 20:01:25 +0900

    fix ndic test code: remove assertIn function for Python 2.6

--- ndic/tests/test_ndic.py
+++ ndic/tests/test_ndic.py
@@ -9,16 +9,14 @@ class NdicTestCase(TestCase):
 
     def test_ndic_should_return_english_word_corresponding_to_korean_word(self):
         test_korean_word = "사과"
-        self.assertIn(
-            "apple",
-            ndic(test_korean_word),
+        self.assertTrue(
+            "apple" in ndic(test_korean_word),
         )
 
     def test_ndic_should_return_korean_word_corresponding_to_english_word(self):
         test_english_word = "apple"
-        self.assertIn(
-            "사과",
-            ndic(test_english_word),
+        self.assertTrue(
+            "사과" in ndic(test_english_word),
         )
     
     def test_ndic_should_return_empty_string_if_nonexistent_korean_word(self):
