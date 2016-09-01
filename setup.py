a36e45a3454c0ee77c5150450739cf994b9a1d0a
jupiny <tmdghks584@gmail.com>
2016-09-02 01:47:07 +0900

    update setup settings: add classifiers and delete dependency_links

--- setup.py
+++ setup.py
@@ -9,26 +9,36 @@ def get_requirements(filename):
     return open(os.path.join(ROOT, filename)).read().splitlines()
 
 setup(
-  name='ndic',
-  # packages = ['ndic'],
-  packages=find_packages(),
-  include_package_data=True,
-  # py_modules=['ndic'],
-  install_requires=get_requirements('requirements.txt'),
-  tests_require=get_requirements('test-requirements.txt'),
-  version=VERSION,
-  description='Python module for NAVER English-Korean and Korean-English dictionaries',
-  long_description=open(os.path.join(ROOT, 'README.md')).read(),
-  author='jupiny',
-  author_email='tmdghks584@gmail.com',
-  url='https://github.com/jupiny/ndic',
-  download_url='https://github.com/jupiny/ndic/tarball/1.0',
-  dependency_links = ['https://github.com/jupiny/ndic/tarball/1.0'],
-  # keywords = ['dictionary', 'translate', 'English', 'Korean', 'Naver'],
-  classifiers=[],
-  test_suite='nose.collector',
-  entry_points='''
-    [console_scripts]
-    ndic=ndic.scripts.search:search
-  ''',
+    name='ndic',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=get_requirements('requirements.txt'),
+    tests_require=get_requirements('test-requirements.txt'),
+    version=VERSION,
+    description='Python module for NAVER English-Korean and Korean-English dictionaries',
+    long_description=open(os.path.join(ROOT, 'README.md')).read(),
+    author='jupiny',
+    author_email='tmdghks584@gmail.com',
+    url='https://github.com/jupiny/ndic',
+    download_url='https://pypi.python.org/pypi/ndic',
+    # keywords = ['dictionary', 'translate', 'English', 'Korean', 'Naver'],
+    test_suite='nose.collector',
+    entry_points='''
+        [console_scripts]
+        ndic=ndic.scripts.search:search
+    ''',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Testing',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
 )
