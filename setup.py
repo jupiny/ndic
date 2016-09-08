130134d3894aece9c81b75549369af273d45b6c3
jupiny <tmdghks584@gmail.com>
2016-09-08 23:40:07 +0900

    upgrade pypi version 1.4 => 1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
-VERSION = '1.4'
+VERSION = '1.5'
 
 def get_requirements(filename):
     return open(os.path.join(ROOT, filename)).read().splitlines()
