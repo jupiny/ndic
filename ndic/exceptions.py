92082a484a07561a92c7a544e5e8f2618620f6a9
jupiny <tmdghks584@gmail.com>
2016-09-10 17:50:01 +0900

    update NdicConnectionError exceptions: add __str__ function again

--- ndic/exceptions.py
+++ ndic/exceptions.py
@@ -17,5 +17,8 @@ class NdicConnectionError(Exception):
     def __init__(self):
         pass
 
+    def __str__(self):
+        return CONNECTION_ERROR_MESSAGE
+
     def __repr__(self):
         return CONNECTION_ERROR_MESSAGE
