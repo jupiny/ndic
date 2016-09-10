ecf9899a688bd9453117160e71666142b05bf6b3
jupiny <tmdghks584@gmail.com>
2016-09-10 17:37:54 +0900

    update NdicConnectionError exceptions: remove __str__ function

--- ndic/exceptions.py
+++ ndic/exceptions.py
@@ -17,8 +17,5 @@ class NdicConnectionError(Exception):
     def __init__(self):
         pass
 
-    def __str__(self):
-        return CONNECTION_ERROR_MESSAGE
-
     def __repr__(self):
         return CONNECTION_ERROR_MESSAGE
