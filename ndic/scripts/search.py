41bd889ddcb4edcea32f06c6ee06801d4ef9bec7
jupiny <tmdghks584@gmail.com>
2018-08-08 23:35:37 +0900

    Add xth option in seach script
    issue #2

--- ndic/scripts/search.py
+++ ndic/scripts/search.py
@@ -14,11 +14,16 @@ from ndic.search import search
 
 @click.command()
 @click.argument('search_word')
-def cli_search(search_word):
+@click.option('--xth', '-x', default=1, help='xth meaning. default=1')
+def cli_search(search_word, xth):
     """
     Search the SEARCH_WORD in English-Korean and Korean-English dictionaries
     and echo the corresponding Korean word(s) or English word(s).
 
     """
-    word_meaning = search(search_word)
+    word_meaning = search(search_word, xth)
     click.echo(word_meaning)
+
+
+if __name__ == '__main__':
+    cli_search()
--- ndic/tests/scripts/test_search.py
+++ ndic/tests/scripts/test_search.py
@@ -68,6 +68,133 @@ class NdicScriptTestCase(TestCase):
             result.output.replace('\n', ''),
         )
 
+    def test_search_korean_word_multiple_meaning(self):
+        test_corresponding_english_word_1 = u"(언어) word, language, speech, " \
+                                            "(literary) tongue"
+        test_corresponding_english_word_2 = u"(동물) horse"
+        test_corresponding_english_word_3 = u"(마지막) end (of), close (of)"
+
+        result1 = self.runner.invoke(
+            cli_search,
+            ["말", "-x", "1"],
+        )
+        self.assertEqual(
+            result1.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result1.output.replace('\n', ''),
+            test_corresponding_english_word_1,
+        )
+
+        result2 = self.runner.invoke(
+            cli_search,
+            ["말", "--xth", "2"],
+        )
+        self.assertEqual(
+            result2.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result2.output.replace('\n', ''),
+            test_corresponding_english_word_2,
+        )
+
+        result3 = self.runner.invoke(
+            cli_search,
+            ["말", "-x", "3"],
+        )
+        self.assertEqual(
+            result3.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result3.output.replace('\n', ''),
+            test_corresponding_english_word_3,
+        )
+
+    def test_search_english_word_multiple_meaning(self):
+        test_corresponding_korean_word_1 = u"받다"
+        test_corresponding_korean_word_2 = u"얻다, 입수하다; 가지다(obtain)"
+        test_corresponding_korean_word_3 = u"(동물의) 새끼; 새끼를 낳음"
+
+        result1 = self.runner.invoke(
+            cli_search,
+            ["get", "-x", "1"],
+        )
+        self.assertEqual(
+            result1.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result1.output.replace('\n', ''),
+            test_corresponding_korean_word_1,
+        )
+
+        result2 = self.runner.invoke(
+            cli_search,
+            ["get", "--xth", "2"],
+        )
+        self.assertEqual(
+            result2.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result2.output.replace('\n', ''),
+            test_corresponding_korean_word_2,
+        )
+
+        result3 = self.runner.invoke(
+            cli_search,
+            ["get", "-x", "3"],
+        )
+        self.assertEqual(
+            result3.exit_code,
+            0,
+        )
+        self.assertEqual(
+            result3.output.replace('\n', ''),
+            test_corresponding_korean_word_3,
+        )
+
+    def test_search_xth_exceed(self):
+        result = self.runner.invoke(
+            cli_search,
+            ["말", "-x", "10"],
+        )
+        self.assertEqual(
+            result.exit_code,
+            0,
+        )
+        self.assertFalse(
+            result.output.replace('\n', ''),
+        )
+
+    def test_search_negative_or_zero_xth(self):
+        result1 = self.runner.invoke(
+            cli_search,
+            ["말", "-x", "0"],
+        )
+        self.assertEqual(
+            result1.exit_code,
+            0,
+        )
+        self.assertFalse(
+            result1.output.replace('\n', ''),
+        )
+
+        result2 = self.runner.invoke(
+            cli_search,
+            ["말", "-x", "-1"],
+        )
+        self.assertEqual(
+            result2.exit_code,
+            0,
+        )
+        self.assertFalse(
+            result2.output.replace('\n', ''),
+        )
+
     @mock.patch.object(requests, 'get', side_effect=requests.ConnectionError)
     def test_search_without_internet_network(self, mock_requests):
         result = self.runner.invoke(
